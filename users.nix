42910b1f76b22c82c0f9855770633ce786cbad27
Sean Gillespie <sean@mistersg.net>
2018-04-17 17:39:20 -0400

    Update user groups

--- users.nix
+++ users.nix
@@ -5,12 +5,20 @@
   users.extraUsers = {
     agillespie = {
       isNormalUser = true;
-      extraGroups = ["wheel" "networkmanager"];
+      extraGroups = [
+        "docker"
+        "wheel"
+        "networkmanager"
+      ];
     };
 
     sgillespie = {
       isNormalUser = true;
-      extraGroups = ["wheel" "networkmanager"];
+      extraGroups = [
+        "docker"
+        "wheel"
+        "networkmanager"
+      ];
       shell = pkgs.zsh;
     };
   };
