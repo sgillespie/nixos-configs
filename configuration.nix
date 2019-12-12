a91c2974fea16fcc7f1bd2865da6ce97b045516c
Sean D Gillespie <sean@mistersg.net>
2019-12-11 20:41:32 -0500

    Update system configuration
     * canTouchEfiVariables = false
     * Bump system version

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
 
   # Use the grub EFI boot loader.
   boot.loader = {
-    efi.canTouchEfiVariables = true;
+    efi.canTouchEfiVariables = false;
     grub = {
       device = "nodev";
       efiSupport = true;
@@ -76,5 +76,5 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "18.09"; # Did you read the comment?
+  system.stateVersion = "19.09"; # Did you read the comment?
 }
