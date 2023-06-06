b41a1a02eec14bf3dbd6fe5d96e7532e9fd4902e
Sean D Gillespie <sean@mistersg.net>
2023-06-05 21:42:23 -0400

    feat(haskell): Add my cachix instance

--- modules/haskell/default.nix
+++ modules/haskell/default.nix
@@ -6,10 +6,13 @@
     keep-derivations = true;
 
     trusted-public-keys = [
+      "sgillespie.cachix.org-1:Zgif/WHW2IzHqbMb1z56cMmV5tLAA+zW9d5iB5w/VU4="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     ];
 
     substituters = [
+      "https://sgillespie.cachix.org"
+      "https://hydra.iohk.io"
       "https://cache.iog.io"
     ];
   };
