74ca1d2b292b9cbcca5a7bbbfc263ea1ee349592
Sean D Gillespie <sean@mistersg.net>
2023-12-22 10:12:27 -0500

    refactor(home-manager): Reorganize XDG_CONFIG files

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -7,14 +7,9 @@
     users.sgillespie = {
       home.stateVersion = "23.11";
 
-      home.file = {
-        ".gitconfig".source = ./gitconfig;
-      };
-
       xdg.configFile = {
-        "kitty/kitty.conf".source = ./kitty.conf;
-        "git/iohk.inc".source = ./gitconfig-iohk;
-        "git/hooks/pre-push".source = ./pre-push;
+        "git".source = ./config/git;
+        "kitty".source = ./config;
       };
     };
   };
