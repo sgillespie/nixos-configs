2f0c8d19245ebb71e1f65b1892dcb722caa2fc83
Sean D Gillespie <sean@mistersg.net>
2025-02-26 13:39:09 -0500

    refactor(attic): Set secrets owner atticd:atticd

--- modules/attic/default.nix
+++ modules/attic/default.nix
@@ -20,13 +20,13 @@ with lib;
           #
           # If 0, chunking is disabled entirely for newly-uploaded NARs.
           # If 1, all NARs are chunked.
-          nar-size-threshold = 64 * 1024; # 64 KiB
+          nar-size-threshold = 128 * 1024; # 128 KiB
 
           # The preferred minimum size of a chunk, in bytes
-          min-size = 16 * 1024; # 16 KiB
+          min-size = 64 * 1024; # 64 KiB
 
           # The preferred average size of a chunk, in bytes
-          avg-size = 64 * 1024; # 64 KiB
+          avg-size = 128 * 1024; # 128 KiB
 
           # The preferred maximum size of a chunk, in bytes
           max-size = 256 * 1024; # 256 KiB
@@ -34,12 +34,18 @@ with lib;
       };
     };
 
+    users.extraUsers.atticd = {
+      isSystemUser = true;
+      group = "atticd";
+    };
+    users.groups.atticd = {};
+
     environment.systemPackages = [ pkgs.attic-client ];
 
     sops.secrets = {
       "attic/environment" = {
         inherit sopsFile;
-        # owner = "atticd";
+        owner = "atticd";
       };
     };
   };
