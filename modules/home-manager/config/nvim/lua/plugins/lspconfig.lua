28d17bd95db124f1d7ec6ce6323c2f94094987fc
Sean D Gillespie <sean@mistersg.net>
2025-03-24 21:55:09 -0400

    fix(home-manager): Fix the haskell lsp config

--- modules/home-manager/config/nvim/lua/plugins/lspconfig.lua
+++ modules/home-manager/config/nvim/lua/plugins/lspconfig.lua
@@ -2,11 +2,10 @@ return {
   "neovim/nvim-lspconfig",
   priority = 1000,
   lazy = false,
-  opts = {
-    servers = {
-      hls = { 
-        filetypes = { 'haskell', 'lhaskell', 'cabal' },
-      },
-    },
-  },
+  config = function(_, opts)
+    require('lspconfig')['hls'].setup{
+      filetypes = { 'haskell', 'lhaskell', 'cabal' },
+      cmd = { "haskell-language-server", "--lsp" };
+    }
+  end,
 }
