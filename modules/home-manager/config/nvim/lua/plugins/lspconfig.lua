3aa90950de8d6093c541482ff6bb9d557b13e5d2
Sean D Gillespie <sean@mistersg.net>
2025-04-02 14:03:48 -0400

    chore(home-manager): Restore global lsp mappings

--- modules/home-manager/config/nvim/lua/plugins/lspconfig.lua
+++ modules/home-manager/config/nvim/lua/plugins/lspconfig.lua
@@ -2,6 +2,7 @@ return {
   "neovim/nvim-lspconfig",
   priority = 1000,
   lazy = false,
+
   config = function(_, opts)
     require('lspconfig')['hls'].setup{
       filetypes = { 'haskell', 'lhaskell' },
@@ -14,4 +15,10 @@ return {
       },
     }
   end,
+
+  keys = {
+    { "grn", function() vim.lsp.buf.rename() end, desc = "LSP Rename" },
+    { "gra", function() vim.lsp.buf.code_action() end, desc = "LSP Code Actions" },
+    { "gq", function() vim.lsp.buf.format() end, desc = "LSP Format" },
+  },
 }
--- modules/home-manager/config/nvim/lua/plugins/snacks.lua
+++ modules/home-manager/config/nvim/lua/plugins/snacks.lua
@@ -69,13 +69,10 @@ return {
     -- LSP
     { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
     { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-    { "gQ", function() vim.lsp.buf.format() end, desc = "LSP Format" },
-    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
     { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
     { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
     { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
     { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
-    { "gO", function() vim.lsp.buf.hover() end, desc = "LSP Hover" },
 
     -- Other
     { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
