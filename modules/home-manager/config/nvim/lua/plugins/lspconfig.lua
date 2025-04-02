21e16e5fe5e450af4a082258c302f893e24e6660
Sean D Gillespie <sean@mistersg.net>
2025-04-01 21:36:53 -0400

    chore(home-manager): Update vim lspconfig

--- modules/home-manager/config/nvim/lua/plugins/lspconfig.lua
+++ modules/home-manager/config/nvim/lua/plugins/lspconfig.lua
@@ -4,8 +4,14 @@ return {
   lazy = false,
   config = function(_, opts)
     require('lspconfig')['hls'].setup{
-      filetypes = { 'haskell', 'lhaskell', 'cabal' },
+      filetypes = { 'haskell', 'lhaskell' },
       cmd = { "haskell-language-server", "--lsp" };
+      settings = {
+        haskell = {
+          cabalFormattingProvider = "cabal-gild",
+          formattingProvider = "fourmolu",
+        },
+      },
     }
   end,
 }
