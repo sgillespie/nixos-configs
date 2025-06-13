1f6762410ba3f26ee2effe516aaca915f168478f
Sean D Gillespie <sean@mistersg.net>
2025-06-13 10:36:11 -0400

    fix(nvim): Temporarily pin neogit to an older commit

--- modules/home-manager/config/nvim/lua/plugins/git.lua
+++ modules/home-manager/config/nvim/lua/plugins/git.lua
@@ -1,6 +1,7 @@
 return {
   {
     "NeogitOrg/neogit",
+    commit = "9bb1e73c534f767607e0a888f3de4c942825c501",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
