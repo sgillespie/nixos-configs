5bbb3dca6c9e17a766bc82039e40f532b2d89f4c
Sean D Gillespie <sean@mistersg.net>
2025-09-12 21:35:46 -0400

    Remove custom xkeyboardconfig patch
    This was initially created due to changes that broke emacs. It is now
    conflicting with some bugfixes in nixpkgs. However, since I'm not using
    emacs anymore, it's easier just to entirely remove that.

--- modules/xserver/xkb.nix
+++ modules/xserver/xkb.nix
@@ -3,22 +3,12 @@
 let
   cfg = config.services.xserver;
 
-  xkb_custom = pkgs.xorg.xkeyboardconfig_custom {
-    layouts = config.services.xserver.xkb.extraLayouts;
-  };
-
 in 
 
 with lib;
 
 {
   config = mkIf cfg.enable {
-    environment.sessionVariables = {
-      # runtime override supported by multiple libraries e. g. libxkbcommon
-      # https://xkbcommon.org/doc/current/group__include-path.html
-      XKB_CONFIG_ROOT = config.services.xserver.xkb.dir;
-    };
-
     services.xserver.xkb = {
       layout = "3l-emacs";
       options = "terminate:ctrl_alt_bksp,ctrl:swapcaps";
