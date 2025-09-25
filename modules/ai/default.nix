85bfa3e4bf731e23f854e66884c97af7e8822782
Sean D Gillespie <sean@mistersg.net>
2025-09-25 08:34:31 -0400

    Add quantized q6_K variants to finetuned ai models
    These models have less compression that the default q4_K_M quantized
    models, which should result in overall better performance. I have
    observed they are reasonably fast on my GPUs, so there's no reason not
    to use them by default.

--- modules/ai/default.nix
+++ modules/ai/default.nix
@@ -19,13 +19,16 @@ with lib;
         enable = true;
         acceleration = "cuda";
         loadModels = [
-          "llama2:7b-chat"
-          "llama3.1:8b-instruct-q4_K_M"
           "llama3.1:8b"
+          "llama3.1:8b-instruct-q6_K"
           "mistral:7b"
-          "mistral:7b-instruct"
+          "mistral:7b-instruct-q6_K"
+          "llama2:7b-chat"
+          "llama2:7b-chat-q6_K"
           "codellama:7b-instruct"
+          "codellama:7b-instruct-q6_K"
           "zephyr:7b-beta"
+          "zephyr:7b-beta-q6_K"
         ];
       };
 
